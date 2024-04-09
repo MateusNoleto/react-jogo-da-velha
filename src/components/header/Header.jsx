e01bdf5749160683633f1466890e4c77b52d6be7
Mateus Noleto <mateusnoletods@gmail.com>
2024-04-08 22:13:15 -0300

    criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
-import Title from "../title/title"
+import Title from "../title/Title"
 import Subtitle from "../subtitle/Subtitle"
 import styles from "./Header.module.css"
+import Icon from "../icon/Icon"
 
 
 function Header() {
@@ -8,6 +9,9 @@ function Header() {
         <div className={styles.header}>
             <Title>Jogo da Velha</Title>
             <Subtitle>criado por Mateus Noleto</Subtitle>
+            <div className={styles.iconContent}>
+                <Icon iconName="github" link= "https://github.com/MateusNoleto"/>
+            </div>
         </div>
     )
 }
